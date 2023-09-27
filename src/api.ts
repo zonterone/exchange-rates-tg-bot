21ccc2545bb7d1008b4341ce22ee0ee8a0e18d54
Andrey Tsivtsivadze <zonterone@gmail.com>
2023-09-27 19:47:55 +0400

    fix: koronapay headers

--- src/api.ts
+++ src/api.ts
@@ -1,110 +1,112 @@
 import ky from 'ky'
 
 const api = ky.create({
-	retry: {
-		limit: 3,
-		backoffLimit: 5000,
-	},
-	timeout: 60000,
+  retry: {
+    limit: 3,
+    backoffLimit: 5000,
+  },
+  timeout: 60000,
 })
 
 enum currenciesEnum {
-	'GEL' = '981',
-	'USD' = '840',
+  'GEL' = '981',
+  'USD' = '840',
 }
 export const getKoronaPayRates = async (
-	receivingCurrency: keyof typeof currenciesEnum
+  receivingCurrency: keyof typeof currenciesEnum
 ): Promise<number | string> => {
-	const searchParams = new URLSearchParams({
-		sendingCountryId: 'RUS',
-		sendingCurrencyId: '810',
-		receivingCountryId: 'GEO',
-		receivingCurrencyId: currenciesEnum[receivingCurrency],
-		paymentMethod: 'debitCard',
-		receivingAmount: '100',
-		receivingMethod: 'cash',
-		paidNotificationEnabled: 'false',
-	})
-	const res = (await api
-		.get('https://koronapay.com/transfers/online/api/transfers/tariffs', {
-			headers: {
-				authority: 'koronapay.com',
-				accept: 'application/vnd.cft-data.v2.99+json',
-				'accept-language': 'en',
-				dnt: '1',
-				referer: 'https://koronapay.com/transfers/online/',
-				'sec-ch-ua': '"Chromium";v="115", "Not/A)Brand";v="99"',
-				'sec-ch-ua-mobile': '?0',
-				'sec-ch-ua-platform': '"macOS"',
-				'sec-fetch-dest': 'empty',
-				'sec-fetch-mode': 'cors',
-				'sec-fetch-site': 'same-origin',
-				'user-agent':
-					'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
-				'x-application': 'Qpay-Web/3.0',
-			},
-			searchParams: searchParams,
-		})
-		.json()) as any
+  const searchParams = new URLSearchParams({
+    sendingCountryId: 'RUS',
+    sendingCurrencyId: '810',
+    receivingCountryId: 'GEO',
+    receivingCurrencyId: currenciesEnum[receivingCurrency],
+    paymentMethod: 'debitCard',
+    receivingAmount: '100',
+    receivingMethod: 'cash',
+    paidNotificationEnabled: 'false',
+  })
+  const res = (await api
+    .get('https://koronapay.com/transfers/online/api/transfers/tariffs', {
+      headers: {
+        authority: 'koronapay.com',
+        accept: 'application/vnd.cft-data.v2.112+json',
+        'accept-language': 'en',
+        'cache-control': 'no-cache',
+        dnt: '1',
+        pragma: 'no-cache',
+        referer: 'https://koronapay.com/transfers/online/',
+        'sec-ch-ua': 'Chromium";v="117", "Not;A=Brand";v="8',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': 'macOS',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+        'x-application': 'Qpay-Web/3.0',
+      },
+      searchParams: searchParams,
+    })
+    .json()) as any
 
-	return res[0].exchangeRate
+  return res[0].exchangeRate
 }
 
 export const getCBRRates = async (
-	currencies: ('USD' | 'GEL')[]
+  currencies: ('USD' | 'GEL')[]
 ): Promise<(number | string)[]> => {
-	const response = (await api
-		.get('https://www.cbr-xml-daily.ru/daily_json.js', {})
-		.json()) as any
+  const response = (await api
+    .get('https://www.cbr-xml-daily.ru/daily_json.js', {})
+    .json()) as any
 
-	const result = currencies.map((currency) => {
-		return response.Valute[currency].Value
-	})
+  const result = currencies.map((currency) => {
+    return response.Valute[currency].Value
+  })
 
-	return result
+  return result
 }
 
 enum SideEnum {
-	'sell',
-	'buy',
+  'sell',
+  'buy',
 }
 
 enum PaymentTypeEnum {
-	'SBP Fast Bank Transfer' = 14,
-	'Bank of Georgia' = 11,
+  'SBP Fast Bank Transfer' = 14,
+  'Bank of Georgia' = 11,
 }
 
-type ByBitRatesArgType = { type: 'sell' | 'buy' } & ({
-	currency: 'RUB'
-	paymentMethod: 'SBP Fast Bank Transfer'
-} | {
-	currency: 'GEL'
-	paymentMethod: 'Bank of Georgia'
-} |
-{
-	currency: 'USD'
-	paymentMethod: 'Bank of Georgia'
-})
+type ByBitRatesArgType = { type: 'sell' | 'buy' } & (
+  | {
+      currency: 'RUB'
+      paymentMethod: 'SBP Fast Bank Transfer'
+    }
+  | {
+      currency: 'GEL'
+      paymentMethod: 'Bank of Georgia'
+    }
+  | {
+      currency: 'USD'
+      paymentMethod: 'Bank of Georgia'
+    }
+)
 
 export const getByBitRates = async (args: ByBitRatesArgType) => {
+  const response = (await api
+    .post('https://api2.bybit.com/fiat/otc/item/online', {
+      json: {
+        tokenId: 'USDT',
+        currencyId: args.currency,
+        payment: [`${PaymentTypeEnum[args.paymentMethod]}`],
+        side: `${SideEnum[args.type]}`,
+        size: '1',
+        page: '1',
+        amount: '',
+        authMaker: false,
+        canTrade: false,
+      },
+    })
+    .json()) as any
 
-	const response = (await api
-		.post('https://api2.bybit.com/fiat/otc/item/online', {
-			json: {
-				tokenId: "USDT",
-				currencyId: args.currency,
-				payment: [
-					`${PaymentTypeEnum[args.paymentMethod]}`
-				],
-				side: `${SideEnum[args.type]}`,
-				size: "1",
-				page: "1",
-				amount: "",
-				authMaker: false,
-				canTrade: false
-			}
-		})
-		.json()) as any
-
-	return response.result.items[0].price
+  return response.result.items[0].price
 }
